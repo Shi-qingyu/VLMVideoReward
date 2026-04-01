e64510ec9883cb85c4eecae1a3b648b22ee492e2
sqy <q.y.shi@stu.pku.edu.cn>
2026-04-01 17:03:08 +0800

    tools

--- tools/upload_hf.py
+++ tools/upload_hf.py
@@ -3,7 +3,7 @@ import argparse
 from huggingface_hub import HfApi
 
 
-HF_TOKEN = "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"
+# HF_TOKEN = "_xaCaAFWPmlUgIMIwMPtwqjVfaTvTlwuidu"
 REPO_ID = "QingyuShi/videoreward"
 REPO_TYPE = "dataset"  # change to "model" if your repo is a model repo
 
