8511fe1aa5df6a300d934497ac8eb487cc4db916
sqy <q.y.shi@stu.pku.edu.cn>
2026-05-13 10:51:59 +0800

    eval

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,3 +1,3 @@
 python evaluation.py \
-    --model_path output/qwen3vl-2b-baseline-1e-bs4-ga4-fps1-t-polished-v3-457 \
-    --dataset_use videoreward_eval_polished_v3
+    --model_path output/qwen3vl-2b-vjepa21-distill-weight1.0-vision-lr5e-5-bs4-ga4-t-merged \
+    --dataset_use videoreward_eval_merged
